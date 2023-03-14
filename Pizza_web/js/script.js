ac7a81fe888437cf6b9f0d32d0df4c31a052a663
subha012 <89789755+subha012@users.noreply.github.com>
2023-03-14 14:21:03 +0530

    Add files via upload

---
+++ Pizza_web/js/script.js
@@ -0,0 +1,87 @@
+let navbar = document.querySelector('.header .flex .navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+   navbar.classList.toggle('active');
+}
+
+let account = document.querySelector('.user-account');
+
+document.querySelector('#user-btn').onclick = () =>{
+   account.classList.add('active');
+}
+
+document.querySelector('#close-account').onclick = () =>{
+   account.classList.remove('active');
+}
+
+let myOrders = document.querySelector('.my-orders');
+
+document.querySelector('#order-btn').onclick = () =>{
+   myOrders.classList.add('active');
+}
+
+document.querySelector('#close-orders').onclick = () =>{
+   myOrders.classList.remove('active');
+}
+
+let cart = document.querySelector('.shopping-cart');
+
+document.querySelector('#cart-btn').onclick = () =>{
+   cart.classList.add('active');
+}
+
+document.querySelector('#close-cart').onclick = () =>{
+   cart.classList.remove('active');
+}
+
+window.onscroll = () =>{
+   navbar.classList.remove('active');
+   myOrders.classList.remove('active');
+   cart.classList.remove('active');
+};
+
+let slides = document.querySelectorAll('.home-bg .home .slide-container .slide');
+let index = 0;
+
+function next(){
+   slides[index].classList.remove('active');
+   index = (index + 1) % slides.length;
+   slides[index].classList.add('active');
+}
+
+function prev(){
+   slides[index].classList.remove('active');
+   index = (index - 1 + slides.length) % slides.length;
+   slides[index].classList.add('active');
+}
+const boxes = document.querySelectorAll(".box"),
+  image = document.querySelector(".image");
+
+//Loop through each boxes element
+boxes.forEach((box) => {
+  //When a draggable element dragged over a box element
+  box.addEventListener("dragover", (e) => {
+    e.preventDefault(); //Prevent default behaviour
+    box.classList.add("hovered");
+  });
+
+  //When a draggable element leaves box element
+  box.addEventListener("dragleave", () => {
+    box.classList.remove("hovered");
+  });
+
+  //When a draggable element is dropped on a box elemen
+  box.addEventListener("drop", () => {
+    box.appendChild(image);
+    box.classList.remove("hovered");
+  });
+});
+
+let accordion = document.querySelectorAll('.faq .accordion-container .accordion');
+
+accordion.forEach(acco =>{
+   acco.onclick = () =>{
+      accordion.forEach(remove => remove.classList.remove('active'));
+      acco.classList.add('active');
+   }
+});
